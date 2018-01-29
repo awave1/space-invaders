0f5162e426149d34be567f6fd8480691ed6da086
awave1 <artgolovin96@gmail.com>
2018-01-28 23:33:40 -0700

    implemented print_num

--- stage2/stage2.c
+++ stage2/stage2.c
@@ -3,6 +3,13 @@
 
 int main() {
 	void *base = Physbase();
+	plot_horizontal_line(base, 39, 96, 39);
+	plot_vertical_line(base, 39, 39, 88);
+
 	print_string(base, 40, 40, "hello string");
+	print_num(base, 40, 50, 9999);
+	print_num(base, 40, 60, 0);
+	print_num(base, 40, 70, 5);
+	print_num(base, 40, 80, 123);
 	return 0;
 }
