6c4231d8fa8898c92fbf112d0a74bcbdf95e3875
awave1 <artgolovin96@gmail.com>
2018-01-29 14:31:52 -0700

    refactor plot_horizontal_line and plot_vertical_line

--- stage2/raster.c
+++ stage2/raster.c
@@ -14,24 +14,14 @@ void plot_pixel(UINT8* base, int x, int y) {
  * Assume y2 > y1
  */ 
 void plot_vertical_line(UINT8* base, int x, int y1, int y2) {
-  int i = 0;
-  if (in_bounds(x, y1) && in_bounds(x, y2)) {
-    for (i = y1; i != y2; i++) {
-      plot_pixel(base, x, i);
-    }
-  }
+  plot_line(base, x, y1, x, y2);
 }
 
 /*
  * Assume x2 > x1
  */ 
 void plot_horizontal_line(UINT8* base, int x1, int x2, int y) {
-  int i = 0;
-  if (in_bounds(x1, y) && in_bounds(x2, y)) {
-    for (i = x1; i != x2; i++) {
-      plot_pixel(base, i, y);
-    }
-  }
+  plot_line(base, x1, y, x2, y);
 }
 
 void plot_line(UINT8* base, int x1, int y1, int x2, int y2) {
