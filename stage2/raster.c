d023703140327da348c4ca5b32e0ff5d63a1fe2a
awave1 <artgolovin96@gmail.com>
2018-01-28 22:30:02 -0700

    added plot_horizontal_line

--- stage2/raster.c
+++ stage2/raster.c
@@ -8,11 +8,26 @@ void plot_pixel(UINT8* base, int x, int y) {
     *(base + y * 80 + (x >> 3)) |= 1 << (7 - (x & 7));
 }
 
+/*
+ * Assume y2 > y1
+ */ 
 void plot_vertical_line(UINT8* base, int x, int y1, int y2) {
   int i = 0;
-  if (in_bounds(x, y1)) {
+  if (in_bounds(x, y1) && in_bounds(x, y2)) {
     for (i = y1; i != y2; i++) {
       plot_pixel(base, x, i);
+    }
+  }
+}
+
+/*
+ * Assume x2 > x1
+ */ 
+void plot_horizontal_line(UINT8* base, int x1, int x2, int y) {
+  int i = 0;
+  if (in_bounds(x1, y) && in_bounds(x2, y)) {
+    for (i = x1; i != x2; i++) {
+      plot_pixel(base, i, y);
     }
   }
 }
--- stage2/stage2.c
+++ stage2/stage2.c
@@ -4,5 +4,6 @@
 int main() {
 	void *base = Physbase();
 	plot_vertical_line(base, 40, 40, 350);
+	plot_horizontal_line(base, 40, 600, 40);
 	return 0;
 }
