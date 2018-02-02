2798312dcc8f4352ec7be64bd63a8146b2bd338c
dartuso <daniel.artuso1@gmail.com>
2018-02-02 10:59:21 -0700

    Created skeleton for stage 3

---
+++ stage3/event.h
@@ -0,0 +1,8 @@
+//
+// Created by dasa9 on 02/02/2018.
+// todo: event handler prototypes go here
+
+#ifndef SPACE_INVADERS_EVENT_H
+#define SPACE_INVADERS_EVENT_H
+
+#endif //SPACE_INVADERS_EVENT_H
