23e9060f6a6434420058762a218a91132bf5489e
dartuso <daniel.artuso1@gmail.com>
2018-02-02 10:59:21 -0700

    Created skeleton for stage 3

---
+++ stage3/model.h
@@ -0,0 +1,8 @@
+//
+// Created by dasa9 on 02/02/2018.
+// todo: object behaviours prototypes go here
+
+#ifndef SPACE_INVADERS_MODEL_H
+#define SPACE_INVADERS_MODEL_H
+
+#endif //SPACE_INVADERS_MODEL_H
