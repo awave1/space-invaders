bf27dfd8339af6488716d30d24deffe2a6545bee
awave1 <artgolovin96@gmail.com>
2018-02-03 01:33:47 -0700

    Modified plot horizontal line

--- stage2/stage2.c
+++ stage2/stage2.c
@@ -1,13 +1,40 @@
 #include <osbind.h>
+#include <stdio.h>
 #include "raster.h"
 #include "bitmap.h"
 
+/*
+ * raster lib test driver
+ */
 int main() {
 	void* base = Physbase();
+	int i, j;
+	const int x_start = 195;
+	const int y_start = 40;
+	int x = x_start;
+	int y = y_start;
 
 	plot_rectangle(base, 0, 0, 640, 400);
-	clear_screen(base);
 
+	/*
+	 * Plot 5 x 8 grid of 16x16 blocks, each 16px apart
+	 */
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 8; j++) {
+			plot_r(base, x, y, 16, 16);
+			x += 32;
+		}
+		x = x_start;
+		y += 32;
+	}
+
+	/*
+	 * Plot approx location of the player's ship
+	 */
+	plot_r(base, 312, 368, 16, 16);
+	
+	getchar();
+	clear_screen(base);
 	return 0;
 }
 
