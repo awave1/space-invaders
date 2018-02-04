3f272f69c5720c00b41b8a3fc468b1fef135d165
awave1 <artgolovin96@gmail.com>
2018-02-03 21:30:01 -0700

    structurize stage3

--- stage3/model.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by dasa9 on 02/02/2018.
-// todo: object behaviours prototypes go here
-
-#ifndef SPACE_INVADERS_MODEL_H
-#define SPACE_INVADERS_MODEL_H
-
-#endif //SPACE_INVADERS_MODEL_H
