44974ab114d7229c12722d2c6a9baf31a34427e7
awave1 <artgolovin96@gmail.com>
2018-02-22 13:20:46 -0700

    implemented inversed clear screen

--- stage2/src/include/raster.h
+++ stage2/src/include/raster.h
@@ -15,22 +15,22 @@
  * Require x,y of pixel to place
  *
  */
-void plot_pixel(UINT8* base, int x, int y);
+void plot_pixel(uint8* base, int x, int y);
 
 /* Plots vertical_line
  * Require x,y of starting location
  * height specifies how many pixels towards the bottom the line will go
  *
  */
-void plot_vertical_line(UINT8* base, int x, int y, int height);
+void plot_vertical_line(uint8* base, int x, int y, int height);
 
 /* Plots horizontal_line
  * Require x,y of starting location
  * Width specifies how many pixels to the right the line will go
  *
  */
-void plot_horizontal_line(UINT8* base, int x, int y, int width);
-void plot_horizontal_line__inverse(UINT8* base, int x, int y, int width);
+void plot_horizontal_line(uint8* base, int x, int y, int width);
+void plot_horizontal_line__inverse(uint8* base, int x, int y, int width);
 
 
 /* Plot line
@@ -38,7 +38,7 @@ void plot_horizontal_line__inverse(UINT8* base, int x, int y, int width);
  * and end line location (x2,y2)
  *
  */
-void plot_line(UINT8* base, int x1, int y1, int x2, int y2);
+void plot_line(uint8* base, int x1, int y1, int x2, int y2);
 
 /* Plot rectangle
  * Require starting coordinates of rectange
@@ -47,43 +47,45 @@ void plot_line(UINT8* base, int x1, int y1, int x2, int y2);
  *  the rectangle will be.
  *
  */
-void plot_rectangle(UINT8* base, int x, int y, int width, int height);
-void plot_rectangle__inverse(UINT8* base, int x, int y, int width, int height);
+void plot_rectangle(uint8* base, int x, int y, int width, int height);
+void plot_rectangle__inverse(uint8* base, int x, int y, int width, int height);
 
 /* Plots 8 by height Bitmap todo: idk if this is correct
  * Requires top left coordinate the bitmap will placed at
  * and which to use
  * Height specifies the height of the bitmap
  */
-void plot_bitmap_8(UINT8* base, int x, int y, UINT8* bitmap, int height);
+void plot_bitmap_8(uint8* base, int x, int y, uint8* bitmap, int height);
 
 /* Plots an 16 by height sized bitmap todo: idk if this is correct
  * Requires top left coordinate the bitmap will placed at
  * and which to use
  * Height specifies the height of the bitmap
  */
-void plot_bitmap_16(UINT16* base, int x, int y, UINT16* bitmap, int height);
+void plot_bitmap_16(uint16* base, int x, int y, uint16* bitmap, int height);
 
 /* Prints a character
  * prints a specified character at given x,y coordinates
  */
-void print_char(UINT8* base, int x, int y, char ch);
+void print_char(uint8* base, int x, int y, char ch);
 
 /* Prints a string (array of char)
  * Prints an array of char at specified x,y coordinates
  */
-void print_string(UINT8* base, int x, int y, char* str);
+void print_string(uint8* base, int x, int y, char* str);
 
 /* Prints a number
  * Prints number or numbers at specified x,y coordinates
  * Limitations:
  *  No negative or decimal numbers - only natural numbers
  */
-void print_num(UINT8* base, int x, int y, UINT16 num);
+void print_num(uint8* base, int x, int y, uint16 num);
 
 /*
  * Clears the entire screen
  */
-void clear_screen(UINT32* base);
+void clear_screen(uint32* base);
+void clear_screen__inverse(uint32* base);
+void _clear_screen(uint32 *base, bool inverse);
 
 #endif
--- stage4/src/stage4.c
+++ stage4/src/stage4.c
@@ -6,7 +6,7 @@ int main() {
   uint16 *base = (uint16*) Physbase();
   uint32 *screen = (uint32) Physbase();
   on_game_start(&model);
-  clear_screen(base);
-  render(&model, base);
+  clear_screen__inverse(base);
+  /* render(&model, base); */
   return 0;
 }
