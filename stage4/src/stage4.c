a608886f5d957715c7e7d6b348a3b3b32015b573
dartuso <daniel.artuso1@gmail.com>
2018-02-22 11:52:59 -0700

    now render a single frame completely

--- stage4/src/stage4.c
+++ stage4/src/stage4.c
@@ -4,6 +4,9 @@
 int main() {
   Model model;
   uint16 *base = (uint16*) Physbase();
+  uint32 *screen = (uint32) Physbase();
+  on_game_start(&model);
+  clear_screen(base);
   render(&model, base);
   return 0;
 }
