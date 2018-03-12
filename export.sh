8667378d8d82b506a6746907aff7f7aa3a64cd0d
awave1 <artgolovin96@gmail.com>
2018-03-12 11:05:32 -0600

    added filename option to script

--- export.sh
+++ export.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-if [ "$1" = "-z" ]; then
-    zip -r ./space_invaders_ArtemG_DanielA . -x \*.o \*.git\* \*.vscode\* \*.DS_Store
-elif [ "$1" = "-g" ]; then
-    git archive -o ./space_invaders_ArtemG_DanielA.zip HEAD
+FILENAME=""
+if [ -z "$2" ]; then
+    FILENAME="space_invaders_ArtemG_DanielA"
 else
-    echo "usage: export.sh [arg];"
+    FILENAME="$2"
+fi
+
+if [ "$1" = "-z" ]; then
+    zip -r ./"$FILENAME" . -x \*.o \*.git\* \*.vscode\* \*.DS_Store
+elif [ "$1" = "-g" ]; then
+    git archive -o ./$FILENAME.zip HEAD
+else
+    echo "usage: export.sh [arg] [filename];"
     echo "       -z = zip using zip"
     echo "       -g = zip using git archive"
 fi
