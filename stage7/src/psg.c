413296c840105495fb36349af8499add54f78666
awave1 <artgolovin96@gmail.com>
2018-03-16 01:14:35 -0600

    psg module skeleton

---
+++ stage7/src/psg.c
@@ -0,0 +1,25 @@
+#include "include/psg.h"
+
+void write_psg(int reg, uint8 val) {
+
+}
+
+uint8 read_psg(int reg) {
+
+}
+
+void set_tone(int channel, int tuning) {
+
+}
+
+void set_volume(int channel, int vol) {
+
+}
+
+void enable_channel(int channel, bool tone_on, bool noise_on) {
+
+}
+
+void stop_sound() {
+  
+}
