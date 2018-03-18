0e789c91cdf51e5e13b34c0e1a944d717c6a7da5
awave1 <artgolovin96@gmail.com>
2018-03-18 11:08:20 -0600

    added effects module template

---
+++ stage7/src/include/effects.h
@@ -0,0 +1,16 @@
+#ifndef SPACE_INVADERS_EFFECTS_H
+#define SPACE_INVADERS_EFFECTS_H
+
+#include "psg.h"
+
+/**
+ * Plays shot effect when called
+ */
+void laser_shot_effect();
+
+/**
+ * Plays an explosion effect when called
+ */
+void explosion_effect();
+
+#endif /* SPACE_INVADERS_EFFECTS_H */
