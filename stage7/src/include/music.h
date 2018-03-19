6b2e9f2ab39ac7cb06353faa2cbeb36432266117
awave1 <artgolovin96@gmail.com>
2018-03-19 10:53:46 -0600

    added note constants

--- stage7/src/include/music.h
+++ stage7/src/include/music.h
@@ -3,6 +3,19 @@
 
 #include "types.h"
 
+#define NOTE_C_FLAT 478
+#define NOTE_C 451
+#define NOTE_D_FLAT 428 
+#define NOTE_D 402
+#define NOTE_E_FLAT 379
+#define NOTE_F_FLAT 358
+#define NOTE_F 338
+#define NOTE_G_FLAT 319
+#define NOTE_G 301
+#define NOTE_A_FLAT 284
+#define NOTE_A 268
+#define NOTE_B_FLAT 253
+
 /**
  * Starts playing the song by loading the data for the first note into the PSG
  */
