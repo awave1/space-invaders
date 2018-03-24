01da7b4de0c6c2d3c3170209a9bf3a5de9cb4518
dartuso <daniel.artuso1@gmail.com>
2018-03-24 17:10:08 -0600

    implement some of menu items

---
+++ stage8/src/include/menu.h
@@ -0,0 +1,14 @@
+/*
+ *  File: menu.h
+ *  Authors: Artem Golovin, Daniel Artuso
+ */
+
+#ifndef SPACE_INVADERS_MENU_H
+#define SPACE_INVADERS_MENU_H
+
+#include <osbind.h>
+#include <stdio.h>
+#include "event.h"
+
+
+#endif /*SPACE_INVADERS_MENU_H*/
---
+++ stage8/src/menu.c
@@ -0,0 +1,50 @@
+#include "include/menu.h"
+
+void menu() {
+  /*TODO: draw splash screen*/
+  process_keyboard_choice();
+}
+
+
+void process_keyboard_choice() {
+  unsigned long input;
+  int choice = 1;
+  while (input != ENTER_KEY || input != ESC_KEY) {
+    if (has_user_input()) {
+      input = get_user_input();
+      switch (input) {
+        case UP_ARROW:
+          if (choice > 1)
+            choice--;
+              break;
+        case DOWN_ARROW:
+          if (choice < 3)
+            choice++;
+              break;
+        default:
+          break;
+      }
+      /*TODO: process drawing selection icon*/
+      if (input == ENTER_KEY)
+        select_option(choice);
+      if (input == ESC_KEY)
+        select_option(3);
+    }
+  }
+}
+
+
+void select_option(int choice) {
+  switch (choice) {
+    case 1:
+      game_loop();
+          stop_sound();
+          break;
+    case 2:
+      /*Two player game mode here*/
+    case 3:
+    default:
+      /*Quit option*/
+      break;
+  }
+}
