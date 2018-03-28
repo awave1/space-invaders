ea60a2f16e790ad6b553ec22c0296576b2ba35f2
dartuso <daniel.artuso1@gmail.com>
2018-03-27 21:45:03 -0600

    added dorito and dorito drawer

--- stage8/src/include/menu.h
+++ stage8/src/include/menu.h
@@ -14,11 +14,14 @@
 #include "splash.h"
 #include "space.h"
 #include "raster.h"
+#include "sprites.h"
 
 void menu();
 
 void process_keyboard_choice();
 
 void select_option(int choice);
+
+void draw_choice_selector(int choice);
 
 #endif /*SPACE_INVADERS_MENU_H*/
--- stage8/src/include/splash.h
+++ stage8/src/include/splash.h
@@ -10,6 +10,5 @@
 #include "const.h"
 
 extern uint32 splash_screen[];
-extern uint16 menu_pointer[SPRITE_HEIGHT];
 
 #endif /* SPACE_INVADERS_SPLASH_H */
