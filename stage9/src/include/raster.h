fd7bc951e5463b36e2cf47558a9123e6f5653ecf
awave1 <artgolovin96@gmail.com>
2018-04-09 02:27:18 -0600

    remove some comments

--- stage9/src/include/raster.h
+++ stage9/src/include/raster.h
@@ -90,13 +90,6 @@ void plot_bitmap_8(uint8 *base, int x, int y, uint8 *bitmap, int height);
  */
 void plot_bitmap_16(uint16 *base, int x, int y, uint16 *bitmap, int height);
 
-/**
- * Plots simple mouse pointer as rectangle
- * 
- * @param base - screen
- * @param x    - x position
- * @param y    - y position
- */
 
 void plot_screen(uint32* base, uint32* splash);
 
